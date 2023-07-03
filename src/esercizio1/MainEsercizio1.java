7d05f4e081146fd9508578cafd200521303b9f80
lucawar <lucaguerra92@yahoo.it>
2023-07-03 13:13:00 +0200

    S2-esercizioGiorno1-commit1

---
+++ src/esercizio1/MainEsercizio1.java
@@ -0,0 +1,13 @@
+package esercizio1;
+
+import java.util.Scanner;
+
+public class MainEsercizio1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+	}
+
+}
